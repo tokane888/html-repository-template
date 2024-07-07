164a3b53792063792d32fd9a6345445eec625a68
tokane888 <tokane888@gmail.com>
2024-07-08 07:09:49 +0900

    fix: module_basic.tsのimport文をsort

--- src/module_basic.ts
+++ src/module_basic.ts
@@ -3,6 +3,6 @@
 // 矛盾が生じたため、typescript側でも.js拡張子を
 // 参考) https://qiita.com/masato_makino/items/8451bf4e62ad27823af1#import%E3%81%A8%E6%8B%A1%E5%BC%B5%E5%AD%90
 // 参考) https://github.com/microsoft/TypeScript/issues/40878
-import { showMessage, Util } from "./App.js";
+import { Util, showMessage } from "./App.js";
 
 showMessage();
